9a902c13c82a8bfa0956079b20519385f7422a2d
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2025-02-02 20:38:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Configuración de Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Programacion3Template API",
+        Version = "v1",
+        Description = "API para el examen de Programación 3",
+        Contact = new OpenApiContact
+        {
+            Name = "Agus",
+            Email = "agus@example.com"
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Programacion3Template API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
